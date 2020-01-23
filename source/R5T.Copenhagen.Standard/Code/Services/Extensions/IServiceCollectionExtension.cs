c50462d79e1dfb6115cd35b9d7e48cdfe0405586
MinexAutomation <minex.automation@gmail.com>
2020-01-22 16:35:11 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Copenhagen.Standard/Code/Services/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Canterbury.Standard;
+using R5T.Copenhagen.Default;
+
+
+namespace R5T.Copenhagen.Standard
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddCDrivePathProvider(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<ICDrivePathProvider, CDrivePathProvider>()
+                .AddCDriveNameConvention()
+                ;
+
+            return services;
+        }
+    }
+}
