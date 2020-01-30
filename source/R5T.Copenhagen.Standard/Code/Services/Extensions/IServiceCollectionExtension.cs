f86c1f04230254e7aed75e0d48b05718fd027db3
MinexAutomation <minex.automation@gmail.com>
2020-01-30 01:30:20 -0800

    Updated IServiceCollection extensions for SerivceAction<T> functionality.

--- source/R5T.Copenhagen.Standard/Code/Services/Extensions/IServiceCollectionExtension.cs
+++ source/R5T.Copenhagen.Standard/Code/Services/Extensions/IServiceCollectionExtension.cs
@@ -4,20 +4,30 @@ using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Canterbury.Standard;
 using R5T.Copenhagen.Default;
+using R5T.Dacia;
 
 
 namespace R5T.Copenhagen.Standard
 {
     public static class IServiceCollectionExtension
     {
+        /// <summary>
+        /// Adds the standard <see cref="ICDrivePathProvider"/> implementation.
+        /// </summary>
         public static IServiceCollection AddCDrivePathProvider(this IServiceCollection services)
         {
-            services
-                .AddSingleton<ICDrivePathProvider, CDrivePathProvider>()
-                .AddCDriveNameConvention()
-                ;
+            services.AddDefaultCDrivePathProvider(services.AddCDriveNameConventionAction());
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the standard <see cref="ICDrivePathProvider"/> implementation.
+        /// </summary>
+        public static ServiceAction<ICDrivePathProvider> AddCDrivePathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ICDrivePathProvider>(() => services.AddCDrivePathProvider());
+            return serviceAction;
         }
     }
 }
